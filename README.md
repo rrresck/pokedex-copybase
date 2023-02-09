c42679654b3ad238702fcbe365d454fb39574d6f
Rafael Rodrigues Resck <rrresck@gmail.com>
2023-02-09 16:29:01 -0300

    Processo Seletivo - Copybase

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+Pokedex elaborada para o Processo Seletivo da Copybase.
+
+O desafio
+Você irá construir uma aplicação para listagem e busca de pokemons. É uma aplicação simples, onde iremos buscar por um determinado pokémon, pelo seu nome, para termos acesso às suas propriedades requisitadas através da PokeAPI v2.
+
 # pokedex-copybase
 
 ## Project setup
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,124 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-container>
+      <v-container>
+        <v-row>
+          <v-container>
+            <v-img
+              :src="require('../src/assets/pokedex.png')"
+              class="my-3"
+              contain
+              height="200"
+            />
+            <h1 class="text-center yellow--text mb-2" style="font-size: 5rem">
+              Pokedex
+            </h1>
+            <h1 class="text-center yellow--text mb-8">
+              Processo Seletivo
+              <a class="black--text" href="https://copybase.com.br">COPYBASE</a>
+            </h1>
+          </v-container>
+        </v-row>
+
+        <v-text-field
+          v-model="search"
+          label="Pesquisar"
+          placeholder="Digite seu Pokemon preferido em minúsculas! Ex: pikachu"
+          solo
+        ></v-text-field>
+
+        <v-row>
+          <v-col
+            cols="5"
+            md="2"
+            v-for="pokemon in filtered_pokemons"
+            :key="pokemon.name"
+          >
+            <PokemonCard :pokemon="pokemon" @clicked="show_pokemon" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-container>
+
+    <PokemonInfoDialog
+      :show.sync="show_dialog"
+      :selected_pokemon="selected_pokemon"
+    />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
+import PokemonCard from "./components/PokemonCard.vue";
+import PokemonInfoDialog from "./components/PokemonInfoDialog.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PokemonCard,
+    PokemonInfoDialog,
+  },
+
+  data() {
+    return {
+      pokemons: [],
+      search: "",
+      show_dialog: false,
+      selected_pokemon: null,
+    };
+  },
+
+  mounted() {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=498")
+      .then((response) => {
+        this.pokemons = response.data.results;
+      });
+  },
+  methods: {
+    show_pokemon(id) {
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
+        this.selected_pokemon = response.data;
+        this.show_dialog = !this.show_dialog;
+      });
+    },
+
+    get_move_level(move) {
+      for (let version of move.version_group_details) {
+        if (
+          version.version_group.name == "sword-shield" &&
+          version.move_learn_method.name == "level-up"
+        ) {
+          return version.level_learned_at;
+        }
+      }
+      return 0;
+    },
+  },
+
+  computed: {
+    filtered_pokemons() {
+      return this.pokemons.filter((item) => {
+        return item.name.includes(this.search);
+      });
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: linear-gradient(
+      to bottom right,
+      rgb(13, 2, 95),
+      rgb(85, 145, 197)
+    )
+    no-repeat center center fixed !important;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover !important;
+  background-position: center;
+  min-height: 100vh;
 }
 </style>
