57fa05755c7c94d37ab35883bcca1fc2cbf86b4f
Rafael Rodrigues Resck <rrresck@gmail.com>
2023-03-02 12:11:13 -0300

    Vue.Config.JS

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+import { defineConfig } from '@vue/cli-service'
+export default defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
 })
